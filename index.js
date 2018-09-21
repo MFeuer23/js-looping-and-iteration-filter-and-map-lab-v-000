ea5ff448ef746684a009c64f5a097b6fabf1cc0a
Mallory Feuer <mallory.feuer@gmail.com>
2018-09-21 13:18:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ function driversWithRevenueOver(array, number) {
   const drivers = array.filter(d => d.revenue > number);
   return drivers;
 }
+
+function driverNamesWithRevenueOver(array, number) {
+  const drivers = driversWithRevenueOver(array, number)
+  const names = drivers.map(d => d.name)
+  return names;
+}
